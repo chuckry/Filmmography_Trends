a4b67ac73e70d76fe177804169b158ceb5c695d9
Chuckry Vengadam <chuckry@umich.edu>
2016-03-28 23:05:43 -0400

    Change file name, modify Person file

--- Person.py
+++ Person.py
@@ -1,3 +1,5 @@
+import operator
+
 class Person:
 	def __init__(self, name, _type, ID):
 		self.name       = name
@@ -21,15 +23,12 @@ class Person:
 		else:
 			self.associates[key] += 1
 
-	'''
-		# Requires: Movie data in the form of a list of lists of dictionaries 
-					(each element being from a different page)
-		# Modifies: Nothing
-		# Effects:	"Flattens" the list of lists of dicts to a list of dicts
-	'''
 	def combine_pages(self):
 		movies = []
 		for page in self.movies:
 			for movie in page['results']:
 				movies.append(movie)
 		self.movies = movies
+
+	def sort_hash(self):
+		return sorted(self.associates.items(), key=operator.itemgetter(1))
